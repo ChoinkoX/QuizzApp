3a85de6d2f9e1d3bbc4264e83dbad953f78c84da
Choinko <dominik-chabro@wp.pl>
2023-07-31 17:17:20 +0200

    rusztowanie

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:secondapp/start_screen.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
