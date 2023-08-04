8cbb21485104adad57093fafe00223c8d6279230
Choinko <dominik-chabro@wp.pl>
2023-08-04 14:28:22 +0200

    add source of image , implement image , add text

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,6 +4,17 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Center(child: Text('Stat Screen'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const Text('learn flutter the fun way!'),
+        ],
+      ),
+    );
   }
 }
