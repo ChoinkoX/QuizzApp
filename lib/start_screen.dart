71b3193d751911530f8096591b1d116f8659eee4
Choinko <dominik-chabro@wp.pl>
2023-08-04 15:57:45 +0200

    add icon

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,7 +12,20 @@ class StartScreen extends StatelessWidget {
             'assets/images/quiz-logo.png',
             width: 300,
           ),
-          const Text('learn flutter the fun way!'),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'learn flutter the fun way!',
+            style: TextStyle(
+                color: Color.fromARGB(255, 232, 128, 192), fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Start Quizz'))
         ],
       ),
     );
