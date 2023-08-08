171d15424ddb9e780e239ebe7d7e5168df4ed04a
Choinko <dominik-chabro@wp.pl>
2023-08-08 20:14:30 +0200

    change child part to widget

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:secondapp/questions_screen.dart';
 import 'package:secondapp/start_screen.dart';
 
 class Quizz extends StatefulWidget {
@@ -10,17 +11,26 @@ class Quizz extends StatefulWidget {
 }
 
 class _QuizzState extends State<Quizz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 39, 2, 46),
-              Color.fromARGB(255, 93, 1, 110),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-            child: const StartScreen()),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 39, 2, 46),
+            Color.fromARGB(255, 93, 1, 110),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: activeScreen,
+        ),
       ),
     );
   }
